1c0cbb355ece7952c44a451cabbb8afeacee903b
Livia Nakashima <43746512+LiviaNakashima@users.noreply.github.com>
2020-11-03 21:45:06 -0300

    Implementando a interface dao

---
+++ app/src/main/java/br/com/wcc/whatdidilearn/data/ItemLearnedDao.kt
@@ -0,0 +1,15 @@
+package br.com.wcc.whatdidilearn.data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import br.com.wcc.whatdidilearn.entities.ItemLearned
+
+@Dao
+interface ItemLearnedDao {
+    @Query("SELECT * FROM learned_item ORDER BY item_title ASC")
+    fun getAll(): List<ItemLearned>
+
+    @Insert
+    fun insert(item: ItemLearned)
+}
--- app/src/main/java/br/com/wcc/whatdidilearn/entities/Level.kt
+++ app/src/main/java/br/com/wcc/whatdidilearn/entities/Level.kt
@@ -1,5 +1,7 @@
 package br.com.wcc.whatdidilearn.entities
 
+import br.com.wcc.whatdidilearn.R
+
 enum class Level {
     HIGH,
     MEDIUM,
